27f2255f3680f99851e496825b1ebf24de5126a8
alvin <alvinecom2018@gmail.com>
2021-09-08 22:38:24 +0700

    fixing home

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,15 @@
 import success from './images/success.png';
 import nijulogo from './icons/niju-logo.png';
 import forgetpass from './images/forget-password.png'
+import logistics from './images/logistics.png'
+import worker from './images/worker.png'
+import quality from './images/quality.png'
 
 export {
     success,
     nijulogo,
-    forgetpass
+    forgetpass,
+    logistics,
+    worker,
+    quality
 };
--- src/pages/HomeScreen/index.js
+++ src/pages/HomeScreen/index.js
@@ -1,22 +1,101 @@
-import { Body, Header, Left, Right, Title } from 'native-base'
+import { Body, Button, Header, Icon, Left, Right, Title } from 'native-base'
 import React, { Component } from 'react'
-import { View } from 'react-native'
+import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native'
+import normalize from 'react-native-normalize'
+import { logistics, nijulogo, quality, worker } from '../../assets'
 
 export default class HomeScreen extends Component{
     render(){
+        const navigation = this.props.navigation;
         return(
-            <View>
-                <Header style={{backgroundColor:'black'}}>
-                    <Left>
+            <View style={{backgroundColor:'#73A3EC', height:'100%'}}>
+                <ScrollView>
+                    {/* Header */}
+                    <View style={{height:normalize(150), backgroundColor:'#5B6BFB', borderBottomRightRadius:50}}>
+                        <View style={{flexDirection:'row', alignItems:'center', paddingTop:normalize(25), paddingLeft:normalize(20)}}>
+                            <Image source={nijulogo} style={{width:normalize(110), height:normalize(100)}} />
+                            <View>
+                                <Text style={{fontFamily:'RedHatDisplay-Regular', color:'white', fontSize:normalize(24), fontWeight:'bold'}}>NIJU</Text>
+                                <Text style={{fontFamily:'RedHatDisplay-Regular', color:'white', fontSize:normalize(20), fontWeight:'bold'}}>SHEARING METAL PLATE</Text>
+                            </View>
+                        </View>
+                    </View>
 
-                    </Left>
-                    <Body>
-                        <Title>NIJU - Shearing</Title>
-                    </Body>
-                    <Right>
+                    <ScrollView horizontal style={{paddingTop:normalize(20)}}>
+                        <View style={{height:normalize(300), backgroundColor:'#5B6BFB'}}>
+                            <Image source={worker} style={{width:normalize(370), height:normalize(270)}}/>
+                        </View>
+                        <View style={{height:normalize(300), backgroundColor:'#5B6BFB'}}>
+                            <Image source={quality} style={{width:normalize(370), height:normalize(270)}}/>
+                        </View>
+                        <View style={{height:normalize(300), backgroundColor:'#5B6BFB'}}>
+                            <Image source={logistics} style={{width:normalize(370), height:normalize(270)}}/>
+                        </View>
+                    </ScrollView>
 
-                    </Right>
-                </Header>
+                    <View style={{paddingTop:normalize(20)}}>
+                        <View style={{height:normalize(600), backgroundColor:'white', borderTopRightRadius:20, borderTopLeftRadius:20}}>
+                            <View style={{alignItems:'center', justifyContent:'center', paddingTop:normalize(10)}}>
+                                <View style={{backgroundColor:'#808080', height:normalize(10), width:normalize(250), borderRadius:10}}/>
+                                <View style={{paddingTop:normalize(20)}}>
+                                    <Text style={{fontFamily:'RedHatDisplay-Regular', fontSize:normalize(24), textAlign:'center'}}>MENU UTAMA</Text>
+
+                                    <View style={{flexDirection:'row', alignItems:'center', justifyContent:'center', paddingTop:normalize(20)}}>
+                                        <TouchableOpacity>
+                                            <View style={{width:normalize(70), height:normalize(70), backgroundColor:'#6D7AF2'}}>
+
+                                            </View>
+                                        </TouchableOpacity>
+                                        <View style={{paddingLeft:normalize(20)}}/>
+                                        <TouchableOpacity>
+                                            <View style={{width:normalize(70), height:normalize(70), backgroundColor:'#54D871'}}>
+
+                                            </View>
+                                        </TouchableOpacity>
+                                        <View style={{paddingLeft:normalize(20)}}/>
+                                        <TouchableOpacity>
+                                            <View style={{width:normalize(70), height:normalize(70), backgroundColor:'#C3B257'}}>
+
+                                            </View>
+                                        </TouchableOpacity>
+                                    </View>
+
+                                    <View style={{flexDirection:'row', alignItems:'center', justifyContent:'center', paddingTop:normalize(20)}}>
+                                        <TouchableOpacity>
+                                            <View style={{width:normalize(70), height:normalize(70), backgroundColor:'#E78181'}}>
+
+                                            </View>
+                                        </TouchableOpacity>
+                                        <View style={{paddingLeft:normalize(20)}}/>
+                                        <TouchableOpacity>
+                                            <View style={{width:normalize(70), height:normalize(70), backgroundColor:'#B4B4B4'}}>
+
+                                            </View>
+                                        </TouchableOpacity>
+                                        <View style={{paddingLeft:normalize(20)}}/>
+                                        <TouchableOpacity>
+                                            <View style={{width:normalize(70), height:normalize(70), backgroundColor:'white'}}>
+
+                                            </View>
+                                        </TouchableOpacity>
+                                    </View>
+
+                                    {/* Button */}
+                                    <View style={{paddingTop:normalize(290)}}>
+                                        <Button onPress={() => navigation.navigate('Login')} full style={{backgroundColor:'#F44444', borderRadius:10, height:normalize(40)}}>
+                                            <Text style={{fontFamily:'RedHatDisplay-Regular', fontSize:normalize(18), textAlign:'center', fontWeight:'bold'}}>KELUAR</Text>
+                                        </Button>
+                                    </View>
+
+                                    {/* copyright */}
+                                    <View style={{paddingTop:normalize(20)}}>
+                                        <Text style={{fontFamily:'RedHatDisplay-Regular', fontSize:normalize(14), textAlign:'center'}}>Copyright By ptniju 2021</Text>
+                                    </View>
+                                </View>
+                            </View>
+                        </View>
+                    </View>
+                </ScrollView>
             </View>
         )
     }
